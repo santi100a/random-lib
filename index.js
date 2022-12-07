c6191f800ec3229104461240f86a0bb5d9745423
santi100a <santyrojasprieto9@gmail.com>
2022-12-06 20:26:14 -0500

    v1.0.5: Bugfix.

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ import { assert, assertType } from '@santi100/assertion-lib';
 
 function random(max, min = 0) {
     assertType(max, 'number'); assertType(min, 'number');
-    assert(min > max && Number.isInteger(max));
+    assert(max > min && Number.isInteger(max), {
+        expected: true,
+        actual: min > max && Number.isInteger(max),
+        operator: '> && Number.isInteger()'
+    });
     if (!min) return Math.floor(Math.random() * max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function randomFloat(max, min = 0.0) {
     assertType(max, 'number'); assertType(min, 'number');
-    assert(min > max);
+    assert(max > min, {
+        expected: true,
+        actual: max > min,
+        operator: '>'
+    });
     if (!min) return Math.floor(Math.random() * max);
     return (Math.random() * (max - min + 1.0)) + min;
 }
