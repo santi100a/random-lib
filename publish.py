fd1b10af9f74cf556114e131e8f3ee21381cf73b
santi100a <santyrojasprieto9@gmail.com>
2022-12-31 13:45:32 -0500

    Let's retry...

--- publish.py
+++ publish.py
@@ -9,8 +9,11 @@ for line in stdin:
 s = s.strip()
 
 if not s:
+    print('received empty string')
+    print(f'{s =}')
     exit(0)
 else:
+    print('received data. publishing...')
     system('yarn publish --access=public --non-interactive')
     exit(0)
 
